c1d284c1524eddcc93dceca7a71c8072600be1fb
Lior Brauer <liorbrauer@gmail.com>
2022-11-20 10:27:10 +0200

    make sure this works too

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -6,15 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CODESPACE_HOST = ENV['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']
+
 module AnythinkMarket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.api_only = true
 
-    # Allow codespace host
+     # Allowed hosts
     config.hosts << ".anythink.market"
-    config.hosts << ".#{ENV['GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN']}"
+    config.hosts << ".#{CODESPACE_HOST}" if CODESPACE_HOST.present?
 
     config.to_prepare do
       DeviseController.respond_to :html, :json
